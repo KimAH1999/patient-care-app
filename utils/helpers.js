3fe1c94be8ee0a391675dae211e0f9d0a96ba13d
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-12-29 13:30:15 -0800

    helpers page added for future possabilities

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+//helpers commented out for time being, this section can be used to create format for date and time.
+//Often times date and time is an important section for legal reasons and proper work flow for appts/check-in/check-out.
+/*module.exports = {
+    format_time: (date) => {
+        return date.toLocaleTimeString();
+    },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear() + 5
+            }`;
+    },
+};*/
