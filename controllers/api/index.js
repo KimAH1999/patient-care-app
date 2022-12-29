896acfae0435fd9b1a0072529adf548f812cc0c8
Kimberly Aguilar Hermoso <kimaguilar2017@gmail.com>
2022-12-29 10:31:24 -0800

    .get instead of .use

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,7 +6,7 @@ const userRoutes = require('./user-routes');
 //const providerRoutes = require('./provider-routes');
 //const patientRoutes = require('./patient-routes');
 //router.use(); mounts middleware for the routes served by the specific router
-router.use('/user', userRoutes);
+router.get('/user', userRoutes);
 //router.use('/provider', providerRoutes);
 //router.use('/patient', patientRoutes);
 
